ad2cac8f1120c8c120003aa6c45bff02c645722a
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2023-02-22 17:06:21 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "464e01e0d90cc31c9fc558fa792a9559",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
